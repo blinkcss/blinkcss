3dde09d8b90d2823c3c8156d26a462cbb0b17e25
Durk Steed Jr <139193744+durksteedjr@users.noreply.github.com>
2024-06-02 00:37:22 -0400

    fix(@blinkcss/deepmerge): types

--- @blinkcss/deepmerge/src/types/deepmerge.type.ts
+++ @blinkcss/deepmerge/src/types/deepmerge.type.ts
@@ -1,4 +1,4 @@
-export type DeepMerge<T1 extends object, T2 extends object> = {
+export type Deepmerge<T1 extends object, T2 extends object> = {
   [K in keyof T1 | keyof T2]: K extends keyof T2
     ? T2[K]
     : K extends keyof T1
